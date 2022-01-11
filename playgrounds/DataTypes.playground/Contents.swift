345054e34ae74633f302be951c458eed04780bf8
celyes <celyes02@gmail.com>
2022-01-11 13:37:42 +0100

    add datatypes playground

--- playgrounds/DataTypes.playground/Contents.swift
+++ playgrounds/DataTypes.playground/Contents.swift
@@ -1,3 +1,29 @@
-import UIKit
+/*
 
-var greeting = "Hello, playground"
+ Created by SwiftWithEase - 11/01/2021
+ 
+ لاحظ اننا في كل متغير، نضيف بعد الاسم نقطتين و نوع البيانات.
+ 
+*/
+
+
+// Int
+var age: Int = 17
+
+
+// Float
+var pi: Float = 3.14
+
+
+// Double
+var surface: Double = 2.318741
+
+
+// String
+var name: String = "Ilyes"
+
+// Character
+
+var singleCharacter: Character = "a"
+
+// O
